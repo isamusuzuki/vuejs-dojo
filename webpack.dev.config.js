9ab738982f3802faa405c4b2484672772186e43c
Isamu Suzuki <isamusuzuki@outlook.com>
2023-08-11 04:12:55 +0900

    feat: 最初のページ avocado を開発

---
+++ webpack.dev.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        avocado: './src/avocado.ts',
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name].bundle.js',
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'public'),
+        },
+        compress: true,
+        port: 8080,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            },
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+                options: {
+                    appendTsSuffixTo: [/\.vue$/],
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.vue'],
+    },
+    plugins: [new VueLoaderPlugin()],
+}
---
+++ webpack.prod.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        avocado: './src/avocado.ts',
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name].bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            },
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+                options: {
+                    appendTsSuffixTo: [/\.vue$/],
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.vue'],
+    },
+    plugins: [new VueLoaderPlugin()],
+}
