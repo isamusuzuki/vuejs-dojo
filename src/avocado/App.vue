5330ffacc116675560c1df953a90a97419d13251
Isamu Suzuki <isamusuzuki@outlook.com>
2023-08-20 23:14:36 +0900

    refactor: SVGタグをHTMLファイルに移行

--- src/avocado/App.vue
+++ src/avocado/App.vue
@@ -29,13 +29,6 @@
             </div>
         </div>
     </section>
-    <svg style="display: none;">
-        <defs>
-            <symbol id="chevron-left-circle" viewBox="0 0 24 24">
-                <path d="M22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2A10,10 0 0,1 22,12M15.4,16.6L10.8,12L15.4,7.4L14,6L8,12L14,18L15.4,16.6Z"/>
-            </symbol>
-        </defs>
-    </svg>
 </template>
 
 <script setup lang="ts">
--- src/daikon/App.vue
+++ src/daikon/App.vue
@@ -10,13 +10,6 @@
         </div>
     </section>
     <now-loading></now-loading>
-    <svg style="display: none;">
-        <defs>
-            <symbol id="chevron-left-circle" viewBox="0 0 24 24">
-                <path d="M22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2A10,10 0 0,1 22,12M15.4,16.6L10.8,12L15.4,7.4L14,6L8,12L14,18L15.4,16.6Z"/>
-            </symbol>
-        </defs>
-    </svg>
 </template>
 
 <script lang="ts">
