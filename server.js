524f3268fa558e4742a2657b0899f33ed1fbbbfb
Isamu Suzuki <isamusuzuki@outlook.com>
2023-08-29 14:06:10 +0900

    feat: multerのdiskStorageを使う

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express')
+const multer = require('multer')
 const path = require('path')
 
 const app = express()
 const port = 3000
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'temp/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
+const upload = multer({ storage })
+
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.post('/api/ingen/upload', upload.single('uploadFile'), (req, res) =>{
+    res.json({success: true, message: `${req.file.originalname}のアップロードが完了しました`})
+})
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'))
